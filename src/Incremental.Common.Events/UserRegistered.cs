01adbd09693108bb0c7748f4f0103a540e68e3e3
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-11-04 10:46:11 +0100

    First iteration

---
+++ src/Incremental.Common.Events/UserRegistered.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Incremental.Common.Events
+{
+    /// <summary>
+    /// Represents a new user account created.
+    /// </summary>
+    public class UserCreated
+    {
+        /// <summary>
+        /// Id of the new user account.
+        /// </summary>
+        public Guid Id { get; set; }
+    }
+}
