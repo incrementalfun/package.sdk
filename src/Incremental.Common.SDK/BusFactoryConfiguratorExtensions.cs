02a8c43abc678a0f270041b9aaefd6e42c94583f
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-04-11 18:04:51 +0200

    feat(extensions): add BusFactoryConfiguratorExtensions

---
+++ src/Incremental.Common.SDK/BusFactoryConfiguratorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Incremental.Common.Messaging;
+using MassTransit;
+
+namespace Incremental.Common.SDK
+{
+    public static class BusFactoryConfiguratorExtensions
+    {
+        public static void SetMessageConvention<TMessage>(this IBusFactoryConfigurator configurator) where TMessage : Message
+        {
+            configurator.Message<TMessage>(t =>
+                t.SetEntityName(configurator.MessageTopology.EntityNameFormatter.FormatEntityName<TMessage>()));
+        }
+    }
+}
