561787ef48c6c8cab7d58ff022a7aabaaaf76946
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-06-08 19:05:22 +0200

    feat: add extensions for ExternalEvent configuration

--- src/Incremental.Common.SDK/BusFactoryConfiguratorExtensions.cs
+++ src/Incremental.Common.SDK/BusFactoryConfiguratorExtensions.cs
@@ -10,5 +10,10 @@ namespace Incremental.Common.SDK
             configurator.Message<TMessage>(t =>
                 t.SetEntityName(configurator.MessageTopology.EntityNameFormatter.FormatEntityName<TMessage>()));
         }
+        public static void SetExternalEventConvention<TEvent>(this IBusFactoryConfigurator configurator) where TEvent : ExternalEvent
+        {
+            configurator.Message<TEvent>(t =>
+                t.SetEntityName(configurator.MessageTopology.EntityNameFormatter.FormatEntityName<TEvent>()));
+        }
     }
 }
